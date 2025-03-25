b6d3c8e822b3efa6da92587fa21b89a8de80b534
HectorDaniell <hector.oviedo@ucsm.edu.pe>
2025-03-25 01:25:44 -0500

    (New)Instalacion de tailwind

--- wp-content/themes/child-theme/functions.php
+++ wp-content/themes/child-theme/functions.php
@@ -1,6 +1,13 @@
 <?php
+// Cargar estilos del tema padre
 function cargar_estilos_padre() {
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
 }
 add_action('wp_enqueue_scripts', 'cargar_estilos_padre');
+
+// Cargar estilos del Child Theme (Tailwind compilado)
+function cargar_estilos_child_theme() {
+    wp_enqueue_style('tailwind-css', get_stylesheet_uri());
+}
+add_action('wp_enqueue_scripts', 'cargar_estilos_child_theme');
 ?>
